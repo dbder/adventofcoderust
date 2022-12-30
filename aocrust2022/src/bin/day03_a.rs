cfc40dc3e8201b5f3923c11e125aede99762fc5e
Debo <mail@deboder.nl>
2022-12-30 17:36:15 +0100

    2022 day03 cleanup

--- aocrust2022/src/bin/day03_a.rs
+++ aocrust2022/src/bin/day03_a.rs
@@ -1,19 +1,10 @@
 fn main() {
     let answer: u32 = include_str!("day03input")
         .lines()
-        .map(|l| l.split_at(l.len()/2))
-        .map(|l| valueOfCommonCharacter(l) as u32)
+        .map(|l| l.split_at(l.len() / 2))
+        .map(|(a, b)| a.chars().find(|p2| b.to_string().contains(*p2)).unwrap())
+        .map(|c| if c >= 'a' { (c as u8 - b'a') as u32 + 1 } else { (c as u8 - b'A') as u32 + 27 })
         .sum::<u32>();
 
-
     println!("{:?}", answer);
-}
-
-fn valueOfCommonCharacter(l: (&str, &str)) -> u8 {
-    let common = l.0.chars().find(|c| l.1.to_string().contains(*c)).unwrap();
-    if common >= 'A' && common <= 'Z' {
-        return common as u8 - b'A' + 27;
-    }
-    return common as u8 - b'a' + 1;
-
 }
--- aocrust2022/src/bin/day03_b.rs
+++ aocrust2022/src/bin/day03_b.rs
@@ -1,28 +1,22 @@
+use std::collections::HashSet;
+
 fn main() {
     let answer = include_str!("day03input")
         .lines()
+        .map(|s| HashSet::<char>::from_iter(s.chars()))
         .collect::<Vec<_>>()
         .chunks(3)
-        .map(|c| value_of_common_character(c) as u32)
+        .map(|sets| find_common_character(sets))
+        .map(|c| if c >= 'a' { c as u8 - b'a' + 1 } else { c as u8 - b'A' + 27 })
+        .map(|i| i as u32)
         .sum::<u32>();
 
     println!("{:?}", answer);
 }
 
-fn value_of_common_character(c: &[&str]) -> u8 {
-    use std::collections::HashSet;
-
-    let mut a: HashSet<char> = HashSet::from_iter(c[0].chars());
-    let b: HashSet<char> = HashSet::from_iter(c[1].chars());
-    let c: HashSet<char> = HashSet::from_iter(c[2].chars());
-
-    a.retain(|ch| b.contains(ch));
-    a.retain(|ch| c.contains(ch));
-    let mut common = 'a';
-    for x in a { common = x; }
-
-    if common >= 'A' && common <= 'Z' {
-        return common as u8 - b'A' + 27;
-    }
-    return common as u8 - b'a' + 1;
+fn find_common_character(sets: &[HashSet<char>]) -> char {
+    return *sets[0]
+        .iter()
+        .find(|b| sets[1].contains(b) && sets[2].contains(b))
+        .unwrap();
 }
