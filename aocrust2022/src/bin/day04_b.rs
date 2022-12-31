6e9b6cfb8092911c570a0d107220d2b4e39abe48
Debo <mail@deboder.nl>
2022-12-31 12:09:19 +0100

    2022 day04 done

---
+++ aocrust2022/src/bin/day04_b.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let answer = include_str!("day04input")
+        .lines()
+        .map(|s| s.split_once(",").unwrap())
+        .map(|(a, b)| (a.split_once("-").unwrap(), b.split_once("-").unwrap()))
+        .map(|(a, b)| ((a.0.to_string().parse::<u32>().unwrap()..=a.1.to_string().parse::<u32>().unwrap()), (b.0.to_string().parse::<u32>().unwrap()..=b.1.to_string().parse::<u32>().unwrap())))
+        .filter(|(a, b)| ((a.contains(&b.start()) || a.contains(&b.end()))) || ((b.contains(&a.start()) || b.contains(&a.end()))))
+        .count();
+
+    println!("{:?}", answer);
+
+
+
+}
