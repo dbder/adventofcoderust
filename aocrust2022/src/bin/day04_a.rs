230a9ffd3d2a1954edf6443180599cd0fa88d14c
Debo <mail@deboder.nl>
2022-12-31 12:36:04 +0100

    2022 day04 refactor

--- aocrust2022/src/bin/day04_a.rs
+++ aocrust2022/src/bin/day04_a.rs
@@ -1,13 +1,21 @@
+use std::ops::RangeInclusive;
+
 fn main() {
     let answer = include_str!("day04input")
         .lines()
         .map(|s| s.split_once(",").unwrap())
         .map(|(a, b)| (a.split_once("-").unwrap(), b.split_once("-").unwrap()))
-        .map(|(a, b)| ((a.0.to_string().parse::<u32>().unwrap()..=a.1.to_string().parse::<u32>().unwrap()), (b.0.to_string().parse::<u32>().unwrap()..=b.1.to_string().parse::<u32>().unwrap())))
-        .filter(|(a, b)| ((a.contains(&b.start()) && a.contains(&b.end()))) || ((b.contains(&a.start()) && b.contains(&a.end()))))
+        .map(|(a, b)| (to_range(a), to_range(b)))
+        .filter(|(a, b)| contains(a, b) || contains(b, a))
         .count();
-    // .collect::<Vec<_>>();
-
 
     println!("{:?}", answer);
+}
+
+fn contains(a: &RangeInclusive<u32>, b: &RangeInclusive<u32>) -> bool {
+    return b.contains(&a.start()) && b.contains(&a.end());
+}
+
+fn to_range(t: (&str, &str)) -> RangeInclusive<u32> {
+    return t.0.parse::<u32>().unwrap()..=t.1.parse::<u32>().unwrap();
 }
--- aocrust2022/src/bin/day04_b.rs
+++ aocrust2022/src/bin/day04_b.rs
@@ -1,14 +1,18 @@
+use std::ops::RangeInclusive;
+
 fn main() {
     let answer = include_str!("day04input")
         .lines()
         .map(|s| s.split_once(",").unwrap())
         .map(|(a, b)| (a.split_once("-").unwrap(), b.split_once("-").unwrap()))
-        .map(|(a, b)| ((a.0.to_string().parse::<u32>().unwrap()..=a.1.to_string().parse::<u32>().unwrap()), (b.0.to_string().parse::<u32>().unwrap()..=b.1.to_string().parse::<u32>().unwrap())))
-        .filter(|(a, b)| ((a.contains(&b.start()) || a.contains(&b.end()))) || ((b.contains(&a.start()) || b.contains(&a.end()))))
+        .map(|(a, b)| (to_range(a), to_range(b)))
+        .filter(|(a, b)| a.start() <= b.end() && a.end() >= b.start())
         .count();
 
     println!("{:?}", answer);
 
+}
 
-
+fn to_range(t: (&str, &str)) -> RangeInclusive<u32> {
+    return t.0.parse::<u32>().unwrap()..=t.1.parse::<u32>().unwrap();
 }
